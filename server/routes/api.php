77f3be07de2604acff8ffd488629cecde46b2f29
Andreas Feichtinger <andreas@venuzle.at>
2024-10-06 16:21:23 +0200

    fix api path and resource, update dependencies in backend

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/users', [UserController::class, 'getUsers'])->name('get-users');
-Route::get('/user/{id}', [UserController::class, 'getUser'])->name('get-user');
+Route::get('/users/{id}', [UserController::class, 'getUser'])->name('get-user');
