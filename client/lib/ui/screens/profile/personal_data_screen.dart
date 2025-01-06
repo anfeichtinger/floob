70386e0225ce6a5f7c8fa57192d258c6b4c43db3
haringma15 <martin.haring2@edu.fh-joanneum.at>
2025-01-06 22:42:24 +0100

    F-81: Implement basic personal data screen

--- client/lib/ui/screens/profile/personal_data_screen.dart
+++ client/lib/ui/screens/profile/personal_data_screen.dart
@@ -2,21 +2,123 @@ import 'package:flutter/material.dart';
 import 'package:floob/ui/widgets/app_bar_gone.dart';
 import 'package:floob/ui/widgets/header.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-class PersonalDataScreen extends ConsumerWidget {
+class PersonalDataScreen extends ConsumerStatefulWidget {
   const PersonalDataScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _PersonalDataScreenState createState() => _PersonalDataScreenState();
+}
+
+class _PersonalDataScreenState extends ConsumerState<PersonalDataScreen> {
+  Color? _selectedColor;
+  final List<Color> _colors = <Color>[
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.indigo,
+    Colors.purple,
+    Colors.pink,
+    Colors.brown,
+    Colors.grey,
+    Colors.black,
+    Colors.white,
+    Colors.cyan,
+    Colors.lime,
+    Colors.teal,
+    Colors.amber,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle textStyle = TextStyle(
+        fontFamily: 'Nunito', fontSize: 16, fontWeight: FontWeight.bold);
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       extendBody: true,
       appBar: const AppBarGone(),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        physics: const BouncingScrollPhysics(),
-        children: const <Widget>[
-          Header(text: 'Personal Data', hasBackAction: true),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              physics: const BouncingScrollPhysics(),
+              children: <Widget>[
+                Header(text: tr('profile_personal_data'), hasBackAction: true),
+                Center(
+                  child: Image.asset(
+                    'assets/img/logo-full-512x512.png',
+                    width: MediaQuery.of(context).size.width / 3,
+                  ),
+                ),
+                const SizedBox(height: 64),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: tr('profile_email'),
+                    labelStyle: textStyle,
+                    border: const OutlineInputBorder(),
+                  ),
+                  style: textStyle,
+                ),
+                const SizedBox(height: 16),
+                DropdownButtonFormField<Color>(
+                  decoration: InputDecoration(
+                    labelText: tr('profile_fav_color'),
+                    labelStyle: textStyle,
+                    border: const OutlineInputBorder(),
+                  ),
+                  value: _selectedColor,
+                  items: _colors.map((Color color) {
+                    return DropdownMenuItem<Color>(
+                      value: color,
+                      child: Container(
+                        width: 24,
+                        height: 24,
+                        color: color,
+                      ),
+                    );
+                  }).toList(),
+                  onChanged: (Color? newValue) {
+                    setState(() {
+                      _selectedColor = newValue;
+                    });
+                  },
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            width: double.infinity,
+            child: FilledButton(
+              onPressed: () {
+                // TODO: Save action
+              },
+              style: ButtonStyle(
+                minimumSize: WidgetStateProperty.all<Size>(
+                  const Size(double.infinity, 54),
+                ),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+              child: Text(
+                tr('profile_save'),
+                style: textStyle.copyWith(
+                  fontSize: textStyle.fontSize! + 2,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 32),
         ],
       ),
       backgroundColor: Theme.of(context).colorScheme.surface,
