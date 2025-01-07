5321ef707c04553f616310899e76df219b7c8fcd
haringma15 <martin.haring2@edu.fh-joanneum.at>
2025-01-07 14:30:06 +0100

    F-79: Revisions

--- client/lib/config/style.dart
+++ client/lib/config/style.dart
@@ -51,7 +51,7 @@ class Style {
     // Divider
     dividerColor: gray300,
     // Used for shadows and in this project also borders of cards
-    shadowColor: Colors.black.withAlpha(25),
+    shadowColor: Colors.black.withValues(alpha: .1),
     // Specific text styles and fonts
     textTheme: ThemeData.light(useMaterial3: true).textTheme.apply(
           fontFamily: 'Nunito',
@@ -102,7 +102,7 @@ class Style {
     // Divider
     dividerColor: gray600,
     // Used for shadows and in this project also borders of cards
-    shadowColor: Colors.white.withAlpha(25),
+    shadowColor: Colors.white.withValues(alpha: .1),
     // Specific text styles and fonts
     textTheme: ThemeData.dark(useMaterial3: true).textTheme.apply(
           fontFamily: 'Nunito',
